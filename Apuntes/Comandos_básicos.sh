005bdd62f52d8b19444fd1544a6228e293ec1b17
fryntiz <fryntiz@gmail.com>
2017-04-18 19:36:42 +0200

    Base para el script con información básica de comandos

--- Apuntes/Comandos_básicos.sh
+++ Apuntes/Comandos_básicos.sh
@@ -34,7 +34,23 @@ y=$rojo
 echo -e "$azul Comandos básicos de BASH"
 echo ""
 echo -e "$x !!$y Repetir último comando"
+echo -e "$x echo \$SHELL$y Muestra en la pantalla la SHELL que estás usando"
+echo -e "$x echo \$BASH_VERSION$y Muestra la versión de BASH"
+echo -e "$x bash$y expecifica que use bash"
+echo -e "$x whereis bash$y Busca donde está guardado bash en el sistema"
 echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+echo -e "$x  $y "
+
 
 
 
