c5fda48413703c81a5fe79ef558828631824dfb2
fryntiz <fryntiz@gmail.com>
2017-05-05 11:06:05 +0200

    Mejoras y modularización del script

---
+++ Ubiquiti/menu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+function ubiquiti_menu() {
+	echo -e "$rojo Menú principal de ubiquiti$gris"
+	}
--- fryntiz-menu.sh
+++ fryntiz-menu.sh
@@ -24,12 +24,22 @@ gris="\033[0;37m"
 #############################
 DIR_TRABAJO="/home/fryntiz/1_GIT/3-Fryntiz/ShellScript"
 
+#############################
+##    ARCHIVOS EXTERNOS    ##
+#############################
+source $DIR_TRABAJO/Ubiquiti/menu.sh
+
+
 #Entrar en la ruta de trabajo
 cd $DIR_TRABAJO
 
-#Versión
-VERSION=$(cat version)
 
+#############################
+##         VERSIÓN         ##
+#############################
+#Limpia pantalla para mostrar la versión un instante
+clear
+VERSION=$(cat version)
 echo -e "
 $rojo
             #############################
@@ -37,41 +47,68 @@ $rojo
             #############################
 $gris
 "
-
 sleep 4
-while :
-	do
-		clear
-		echo ""
-		echo -e "$rojo <1>$verde Entrar al modo edición de este script$gris"
-		echo -e "$rojo <2>$verde Commit con los cambios en este repositorio$gris"
-		echo -e "$rojo <3>$verde Subir cambios al repositorio$gris"
-		echo -e "$rojo <0>$verde Salir del programa$gris"
-		echo ""
 
-		read -p "Opción elegida + [ENTER]: " opcion
-		case $opcion in
-			1)
-				nano fryntiz-menu.sh
-				;;
+############################
+## EDITAR MENÚ PRINCIPAL  ##
+############################
+# Esta función permite editar el script principal
+# Incluye hacer commit y subir a los repositorios
+function editar() {
+	while true :
+		do
+			clear
+			echo ""
+			echo -e "$rojo <1>$verde Entrar al modo edición de este script$gris"
+			echo -e "$rojo <2>$verde Commit con los cambios en este repositorio$gris"
+			echo -e "$rojo <3>$verde Subir cambios al repositorio$gris"
+			echo -e "$rojo <0>$verde Salir del programa$gris"
+			echo ""
 
-			2)
-				echo "Introduce el comentario para el commit"
-				read comentario
-				git add *
-				git commit -m "$comentario"
-				;;
+			read -p "Opción elegida + [ENTER]: " opcion
+			case $opcion in
+				1)
+					nano fryntiz-menu.sh
+					;;
 
-			3)
-				time git push
-				;;
+				2)
+					echo "Introduce el comentario para el commit"
+					read comentario
+					git add *
+					git commit -m "$comentario"
+					;;
 
-			0)
-				exit 1
-				;;
+				3)
+					time git push
+					;;
 
-			*)
-				echo -e "La opción introducida no es correcta"
-				;;
-		esac
+				0)
+					break
+					;;
+
+				*)
+					echo -e "La opción introducida no es correcta"
+					;;
+			esac
+	done
+}
+
+############################
+##     MENÚ PRINCIPAL     ##
+############################
+while true; do
+	clear
+	echo 1 editar
+	echo 2 ubiquiti
+	echo 0 Salir
+	read principal
+
+	case $principal in
+		1) editar;; #Llamada a la función que contiene el menú para editar, especificada justo arriba
+		2) ubiquiti_menu;;
+		0) exit 1;;
+		*) echo Entrada no válida;;
+
+	esac
 done
+
