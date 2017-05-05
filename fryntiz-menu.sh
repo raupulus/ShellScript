907d82851ffab58bac08668bc17e88a9363a1e56
fryntiz <fryntiz@gmail.com>
2017-05-05 11:41:02 +0200

    Añadidos submenús

--- fryntiz-menu.sh
+++ fryntiz-menu.sh
@@ -28,11 +28,9 @@ DIR_TRABAJO="/home/fryntiz/1_GIT/3-Fryntiz/ShellScript"
 ##    ARCHIVOS EXTERNOS    ##
 #############################
 source $DIR_TRABAJO/Ubiquiti/menu.sh
-
-
-#Entrar en la ruta de trabajo
-cd $DIR_TRABAJO
-
+source $DIR_TRABAJO/Apuntes/menu.sh
+source $DIR_TRABAJO/MiniScripts/menu.sh
+source $DIR_TRABAJO/Pentesting/menu.sh
 
 #############################
 ##         VERSIÓN         ##
@@ -50,7 +48,7 @@ $gris
 sleep 4
 
 ############################
-## EDITAR MENÚ PRINCIPAL  ##
+##     EDITAR SCRIPTS     ##
 ############################
 # Esta función permite editar el script principal
 # Incluye hacer commit y subir a los repositorios
@@ -59,21 +57,28 @@ function editar() {
 		do
 			clear
 			echo ""
-			echo -e "$rojo <1>$verde Entrar al modo edición de este script$gris"
-			echo -e "$rojo <2>$verde Commit con los cambios en este repositorio$gris"
-			echo -e "$rojo <3>$verde Subir cambios al repositorio$gris"
+			echo -e "$rojo <1>$verde Commit con los cambios en este repositorio$gris"
+			echo -e "$rojo <2>$verde Subir cambios al repositorio$gris"
+			echo -e "$rojo <3>$verde Editar este script$gris"
+			echo -e "$rojo <4>$verde Editar menú Apuntes$gris"
+			echo -e "$rojo <5>$verde Editar menú Mini Scripts$gris"
+			echo -e "$rojo <6>$verde Editar menú Pentesting$gris"
+			echo -e "$rojo <x>$amarillo Posiciona en la raíz del repositorio$gris"
 			echo -e "$rojo <0>$verde Volver al menú Principal$gris"
 			echo ""
 
 			read -p "Opción elegida + [ENTER]: " opcion
 			case $opcion in
-				1) nano fryntiz-menu.sh;;
-				2) echo "Introduce el comentario para el commit"
+				1) echo "Introduce el comentario para el commit"
 				   read comentario
 				   git add *
 				   git commit -m "$comentario"
 				   ;;
-				3) time git push;;
+				2) time git push;;
+				3) nano -w $DIR_TRABAJO/fryntiz-menu.sh;;
+				4) nano -w $DIR_TRABAJO/Apuntes/menu.sh;;
+				5) nano -w $DIR_TRABAJO/MiniScripts/menu.sh;;
+				6) nano -w $DIR_TRABAJO/Pentesting/menu.sh;;
 				0) break;;
 				*) echo -e "La opción introducida no es correcta";;
 			esac
