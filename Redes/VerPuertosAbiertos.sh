73eadab195f792ffb7c1e4fe7e722e475f3ebad2
fryntiz <fryntiz@gmail.com>
2017-05-14 03:14:07 +0200

    Añadido filtro para sacar solo puerto y tipo de puerto

--- Redes/VerPuertosAbiertos.sh
+++ Redes/VerPuertosAbiertos.sh
@@ -23,10 +23,22 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
+PUERTO_INICIAL=21
+PUERTO_FINAL=22
+IP=localhost
+
+#Muestra el número de puerto abierto
+PUERTO="nmap -p $PUERTO_INICIAL'-'$PUERTO_FINAL -A -v $IP | grep Discovered | tr -s ' ' | cut -d ' ' -f4 | grep ^[0-9]* | cut -d '/' -f1"
+
+#Muestra el tipo de puerto UTP o TCP
+TIPO="nmap -p $PUERTO_INICIAL'-'$PUERTO_FINAL -A -v $IP | grep Discovered | tr -s ' ' | cut -d ' ' -f4 | grep ^[0-9]* | cut -d '/' -f2"
 
 #Escanea los puertos internos de la red
-echo "Puertos en Local"
-nmap -p 1-65535 -A -v localhost
+echo -e "Buscando puertos abiertos en modo Local"
+echo "`eval $PUERTO` tipo de puerto `eval $TIPO`"
+
+
+
 
 
 #Escanear según la ip del propio equipo
@@ -42,3 +54,4 @@ Mostrar tabla con información clara tanto desde punto de vista local como remot
 Mostrar de forma clara cada puerto abierto
 Mostrar de forma clara el sistema operativo
 COMMENT
+
