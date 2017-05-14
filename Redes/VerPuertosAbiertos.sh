46fc97a9da450e751cd93407bf30a4ed4eb77bc8
fryntiz <fryntiz@gmail.com>
2017-05-14 04:54:37 +0200

    Retoques

--- Redes/VerPuertosAbiertos.sh
+++ Redes/VerPuertosAbiertos.sh
@@ -23,13 +23,9 @@ verde="\033[1;32m"
 #############################
 ##   Variables Generales   ##
 #############################
-PUERTO_INICIAL=21
-PUERTO_FINAL=22
+PUERTO_INICIAL=0
+PUERTO_FINAL=65535
 IP=localhost
-
-#function escanear($i) {
-#	PUERTO="nmap -p $i -A -v $IP | grep Discovered | tr -s ' ' | cut -d ' ' -f4 | grep ^[0-9]*"
-#}
 
 #Escanea los puertos internos de la red
 echo -e "Buscando puertos abiertos en modo Local"
@@ -39,12 +35,7 @@ do
 	echo "Puerto abierto --> `eval $PUERTO`"
 done
 
-#Escanear según la ip del propio equipo
-#El siguiente comando mostrará todas las IP existentes con ifconfig
 #Estoy limitando a la primera, que es la correspondiente a eth0
-
-#DEBIDO A LO ANTERIOR DESCRITO, NO FUNCIONARÁ AÚN POR WIFI
-
 IP=`ifconfig | grep -e 'eth' -e 'wlan' -e 'inet ' | tr -s ' ' | cut -d ' ' -f3 | cut -d ':' -f2 | grep -e "^[0-9]*.[0-9]*.[0-9]*.[0-9]*$"  | grep -v -e ^127.0.0.1$ -e ^0.0.0.0$ -e ^127.0.0.1$ -e ^255.255.255.255$ | head -n 1`
 
 #Los puertos desde fuera pueden ser distintos a los de localhost
@@ -57,11 +48,7 @@ do
 done
 
 
-
 ################## IDEAS DE FUTURO ########################
 <<COMMENT
-Mostrar tabla con información clara tanto desde punto de vista local como remoto
-Mostrar de forma clara cada puerto abierto
-Mostrar de forma clara el sistema operativo
-COMMENT
 
+COMMENT
