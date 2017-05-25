9a83f0295858519afc4960530306cc242b4724e7
fryntiz <fryntiz@gmail.com>
2017-05-26 00:12:30 +0200

    Base para script

---
+++ MiniScripts/ContarLineas.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+# Este script contará las líneas de todos los archivos del directorio
+
