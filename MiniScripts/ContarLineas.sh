ddf9d6751a85ee1fa73624621e8d6283220d403c
fryntiz <fryntiz@gmail.com>
2017-05-26 00:14:41 +0200

    Sacar todas las líneas de todos los archivos en un array

--- MiniScripts/ContarLineas.sh
+++ MiniScripts/ContarLineas.sh
@@ -10,3 +10,7 @@
 
 # Este script contará las líneas de todos los archivos del directorio
 
+TOTAL=0
+
+#Cuenta todas las líneas y las agrego a un array
+TMP=`find  -type f -exec wc -l {} \; | cut -d " " -f 1`
