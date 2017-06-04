99ea1f6f1a41dfcc62c1da2a6c2eef2c87c5ef26
fryntiz <fryntiz@gmail.com>
2017-06-05 01:54:36 +0200

    Rellenar cadena con exclusión de archivos y directorios

--- Sistema/BackupRaíz.sh
+++ Sistema/BackupRaíz.sh
@@ -26,8 +26,8 @@
 ##        VARIABLES       ##
 ############################
 USERNAME="$(whoami)"
-DIR_EXCLUIDOS=""
-ARCHIVOS_EXCLUIDOS=""
+DIR_EXCLUIDOS=("/home" "/usr/src" "/tmp" "/var/log" "/mnt" "/media" "/proc")
+ARCHIVOS_EXCLUIDOS=("lost+found" "Trash" "Cache")
 PASSWORD=""
 TMP=""
 NOMBRE_BACKUP="Backup-$(date +%Y%m%d).tar"
@@ -37,3 +37,24 @@ if [ ! $USERNAME == "root" ]; then
 	echo "Un backup para la raíz del sistema requiere ser usuario \"root\""
 	exit 1
 fi
+
+# Rellenar lista de exclusiones para directorios
+for i in ${DIR_EXCLUIDOS[*]}
+do
+	TMP="$TMP --exclude=$i"
+done
+
+# Añade los valores de TMP de nuevo a DIR_EXCLUIDOS en forma de cadena bien formada
+DIR_EXCLUIDOS=$TMP
+TMP="" # Resetea la variable temporal
+
+# Rellenar lista de exclusiones para archivos
+for i in ${ARCHIVOS_EXCLUIDOS[*]}
+do
+	TMP="$TMP --exclude=$i"
+done
+
+echo
+echo "Archivos --> $TMP"
+echo
+echo "Directorios --> $DIR_EXCLUIDOS"
