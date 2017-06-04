9b332c0acbc94a2952a6403ae0e829762d8b719d
fryntiz <fryntiz@gmail.com>
2017-06-05 01:05:52 +0200

    Establecido funcionamiento del script

--- Sistema/BackupRaíz.sh
+++ Sistema/BackupRaíz.sh
@@ -7,3 +7,24 @@
 ## ##                             ## ##
 # ###       www.fryntiz.es        ### #
 #######################################
+
+# Recuperar --> 7z x backup-*.tar.7z && tar xf backup-*.tar
+
+# Directorios excluidos:
+# /home
+# /usr/src
+# /tmp
+# /var/log
+# /mnt
+# /media
+# /proc
+# "lost+found"
+# "Trash"
+# "Cache"
+
+############################
+##        VARIABLES       ##
+############################
+DIR_EXCLUIDOS=""
+ARCHIVOS_EXCLUIDOS=""
+
