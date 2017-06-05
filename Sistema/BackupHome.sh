424040feffb7d099f17e69d6ba36b5e3ebc1f8f6
fryntiz <fryntiz@gmail.com>
2017-06-05 20:46:04 +0200

    Mejoras

--- Sistema/BackupHome.sh
+++ Sistema/BackupHome.sh
@@ -15,7 +15,7 @@
 ############################
 USERNAME="$(whoami)"
 NOMBRE_BACKUP="Backup_HOME-$(date +%Y%m%d).tar"
-DIR_EXCLUIDOS=("0-MOUNT" "1_GIT" "1-MOUNT" "2_Bases_de_Datos" "3_Librerías" "4_Programas" "5_Entornos_de_Trabajo" "6_Máquinas_Virtuales" "7_Mis_Proyectos" "8_Backups" "9_Dropbox" "10_GoogleDrive" "11_CloudStation" "12_Pentesting" "13_Compartido_Smartphone" "14_CloudStation_Compartido" "Descargas" "Documentos" "Imágenes" "NHCK" "PlayOnLinux's virtual drives" "RastroArtesanal" "repositorio" "TEMPORAL" "temporal" "tmp" "Vídeos" ".cache" ".local/share/Trash" ".PlayOnLinux" ".thumbnails") # Nombres de directorios excluidos dentro del home
+DIR_EXCLUIDOS=("0-MOUNT" "1_GIT" "1-MOUNT" "2_Bases_de_Datos" "3_Librerías" "4_Programas" "5_Entornos_de_Trabajo" "6_Máquinas_Virtuales" "7_Mis_Proyectos" "8_Backups" "9_Dropbox" "10_GoogleDrive" "11_CloudStation" "12_Pentesting" "13_Compartido_Smartphone" "14_CloudStation_Compartido" "Descargas" "Documentos" "Imágenes" "NHCK" "PlayOnLinux's\ virtual\ drives" "RastroArtesanal" "repositorio" "TEMPORAL" "temporal" "tmp" "Vídeos" ".cache" ".local/share/Trash" ".PlayOnLinux" ".thumbnails") # Nombres de directorios excluidos dentro del home
 ARCHIVOS_EXCLUIDOS=("Backup_HOME-*" ".cache" "cache" "lost+found" "Trash" "Cache" ".trash" "trash" ".Trash-1000")
 PASSWORD="none"
 TMP=""
@@ -41,7 +41,7 @@ TMP="" # Resetea la variable temporal
 # Rellenar lista de exclusiones para directorios
 for i in ${DIR_EXCLUIDOS[*]}
 do
-	TMP="$TMP --exclude=\"/home/$USERNAME/$i\""
+	TMP="$TMP --exclude=/home/$USERNAME/$i"
 done
 
 # Añade los valores de TMP de nuevo a DIR_EXCLUIDOS en forma de cadena bien formada
@@ -68,7 +68,7 @@ echo "Comenzará en cuanto pulses intro"
 read TMP
 
 # Empaquetar en tar guardándolo dentro de /tmp
-tar -cpf $RUTA_DESTINO/$NOMBRE_BACKUP -C /home/$USERNAME/* $DIR_EXCLUIDOS $ARCHIVOS_EXCLUIDOS
+tar -cpvf $RUTA_DESTINO/$NOMBRE_BACKUP -C /home/* $DIR_EXCLUIDOS $ARCHIVOS_EXCLUIDOS
 
 # Comprimir y cifrar en 7z (-mhe=on activa cifrado de encabezado, más seguridad)
 7z a $RUTA_DESTINO/$NOMBRE_BACKUP.7z -mhe=on -p$PASSWORD $RUTA_DESTINO/$NOMBRE_BACKUP
