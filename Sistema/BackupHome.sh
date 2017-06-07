78a8b6e86053800dc3ec6c2156ab7c4c65325e24
fryntiz <fryntiz@gmail.com>
2017-06-07 12:58:35 +0200

    Fuera de exclusión Documentos e imágenes

--- Sistema/BackupHome.sh
+++ Sistema/BackupHome.sh
@@ -27,7 +27,7 @@ USUARIOS=`ls /home`
 NOMBRE_BACKUP="Backup_HOME-$(date +%Y%m%d).tar"
 
 # Nombres de directorios excluidos dentro del home, ruta relativa dentro del home de usuario
-DIR_EXCLUIDOS=("0-MOUNT" "1_GIT" "1-MOUNT" "2_Bases_de_Datos" "3_Librerías" "4_Programas" "5_Entornos_de_Trabajo" "6_Máquinas_Virtuales" "7_Mis_Proyectos" "8_Backups" "9_Dropbox" "10_GoogleDrive" "11_CloudStation" "12_Pentesting" "13_Compartido_Smartphone" "14_CloudStation_Compartido" "Descargas" "Documentos" "Imágenes" "NHCK" "PlayOnLinux's\ virtual\ drives" "RastroArtesanal" "repositorio" "TEMPORAL" "temporal" "tmp" "Vídeos" ".cache" ".local/share/Trash" ".PlayOnLinux" ".thumbnails")
+DIR_EXCLUIDOS=("0-MOUNT" "1_GIT" "1-MOUNT" "2_Bases_de_Datos" "3_Librerías" "4_Programas" "5_Entornos_de_Trabajo" "6_Máquinas_Virtuales" "7_Mis_Proyectos" "8_Backups" "9_Dropbox" "10_GoogleDrive" "11_CloudStation" "12_Pentesting" "13_Compartido_Smartphone" "14_CloudStation_Compartido" "Descargas" "NHCK" "PlayOnLinux's\ virtual\ drives" "RastroArtesanal" "repositorio" "TEMPORAL" "temporal" "tmp" "Vídeos" ".cache" ".local/share/Trash" ".PlayOnLinux" ".thumbnails")
 
 # Archivos o directorios excluidos mientras coincida el nombre, estando en cualquier ruta del usuario
 ARCHIVOS_EXCLUIDOS=("Backup_HOME-*" ".cache" "cache" "lost+found" "Trash" "Cache" ".trash" "trash" ".Trash-1000")
