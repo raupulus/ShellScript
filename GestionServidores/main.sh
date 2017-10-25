eb7bc4a5ec685f1fb1d226662b90bbdfe6954fd9
fryntiz <fryntiz@gmail.com>
2017-10-25 02:14:31 +0200

    Base para menú

---
+++ GestionServidores/main.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#######################################
+# ###     Raúl Caro Pastorino     ### #
+## ##                             ## ##
+### # https://github.com/fryntiz/ # ###
+## ##                             ## ##
+# ###       www.fryntiz.es        ### #
+#######################################
+
+#menú desde donde elegir el servicio para activar o apagar
+
+############################
+##   Constantes Colores   ##
+############################
+amarillo="\033[1;33m"
+rojo="\033[1;31m"
+verde="\033[1;32m"
+magenta="\033[1;35m"
+azul="\033[1;34m"
+blanco="\033[1;37m"
+gris="\033[0;37m"
+
+#############################
+##   Variables Generales   ##
+#############################
+
