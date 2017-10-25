94a59c95f86fee04a0f98bd64ce4d52df2ef7370
fryntiz <fryntiz@gmail.com>
2017-10-25 02:31:03 +0200

    Añadidas opciones para apache2

--- GestionServidores/apache2.sh
+++ GestionServidores/apache2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# -*- ENCODING: UTF-8 -*-
 #######################################
 # ###     Raúl Caro Pastorino     ### #
 ## ##                             ## ##
@@ -8,19 +8,74 @@
 # ###       www.fryntiz.es        ### #
 #######################################
 
-############################
-##   Constantes Colores   ##
-############################
-amarillo="\033[1;33m"
-rojo="\033[1;31m"
-verde="\033[1;32m"
-magenta="\033[1;35m"
-azul="\033[1;34m"
-blanco="\033[1;37m"
-gris="\033[0;37m"
-
 #############################
 ##   Variables Generales   ##
 #############################
 
+# Menu
+while :
+    do
+        clear
 
+        echo ""
+        echo -e "             $amarillo Apache2 $rojo   $version"
+        echo ""
+        echo -e "   $rojo 1)  $verde Deshabilitar del inicio"
+        echo -e "   $rojo 2)  $verde Habilitar en el inicio"
+        echo -e "   $rojo 3)  $verde Apagar"
+        echo -e "   $rojo 4)  $verde Encender"
+        echo -e "   $rojo 5)  $verde Reiniciar"
+        #echo -e "   $rojo 6)  $verde Reinicio Seguro"
+        echo -e "   $rojo 0)  $verde Volver atrás$gris"
+        echo ""
+
+    read -p "  → " OPCION
+    case $OPCION in
+
+        1)  # Deshabilitar del inicio
+            clear
+            sudo systemctl disable apache2
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        2)  # Habilitar al inicio
+            clear
+            sudo systemctl enable apache2
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        3)  # Apagar
+            clear
+            sudo systemctl stop apache2
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        4)  # Encender
+            clear
+            sudo systemctl start apache2
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        5)  # Reiniciar
+            clear
+            sudo systemctl restart apache2
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+        6)  # Reinicio Seguro
+            clear
+            read -p "Pulsa una tecla para continuar" foo
+            continue;;
+
+
+        0)  # Volver Atrás
+            clear
+            break;;
+
+        *)  # Cualquier otra opción que no sea las anteriores
+            clear
+            echo ""
+            echo -e "$rojo La opción elegida no es válida$amarillo introduce otra"
+            read -p "Pulsa una tecla para continuar" foo;;
+    esac
+done
