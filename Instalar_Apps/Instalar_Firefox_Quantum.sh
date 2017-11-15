17f79baeb496cb38af624bcd1af06a9b401827a4
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-16 00:10:07 +0100

    Reajustes

--- Instalar_Apps/Instalar_Firefox_Quantum.sh
+++ Instalar_Apps/Instalar_Firefox_Quantum.sh
@@ -50,37 +50,37 @@ then
 fi
 
 
-# Firefox Quantum Edition
-function firefox_quantumr() {
+# Firefox Developer Edition
+function firefox_developer() {
 
     function instalar() {
-        # Desempaquetar Firefox-Quantum_amd64.tar.bz2
-        mkdir -p /tmp/Firefox-Quantum-Developer_amd64 2>> /dev/null
-        tar -xjvf /tmp/Firefox-Quantum-Developer_amd64.tar.bz2 -C /tmp/Firefox-Quantum-Developer_amd64 2>> /dev/null
+        # Desempaquetar Firefox-Developer_amd64.tar.bz2
+        mkdir -p /tmp/Firefox-Developer_amd64 2>> /dev/null
+        tar -xjvf /tmp/Firefox-Developer_amd64.tar.bz2 -C /tmp/Firefox-Developer_amd64 2>> /dev/null
 
         # Mover archivo extraido a su ubicación final
-        mv /tmp/Firefox-Quantum-Developer_amd64/firefox ~/.local/opt/Firefox_Quantum_Developer 2>> /dev/null
+        mv /tmp/Firefox-Developer_amd64/firefox ~/.local/opt/Firefox_Developer 2>> /dev/null
 
         # Crear enlaces de usuario y permisos de ejecución
-        echo "$HOME/.local/opt/Firefox_Quantum_Developer/firefox - P Firefox-Quantum" > ~/.local/bin/firefox-quantum
+        echo "$HOME/.local/opt/Firefox_Developer/firefox" > ~/.local/bin/firefox-developer
         # ln -s ~/.local/opt/Firefox_Quantum_Developer/firefox ~/.local/bin/firefox-quantum 2>> /dev/null
-        chmod +x ~/.local/bin/firefox-quantum
+        chmod +x ~/.local/bin/firefox-developer
     }
 
-    if [ -f ~/.local/bin/firefox-quantum ]
+    if [ -f ~/.local/bin/firefox-developer ]
     then
         echo -e "$verde Ya esta$rojo Firefox Quantum Developer Edition$verde instalado en el equipo, omitiendo paso$gris"
     # Comprueba que no está el archivo descargado en este directorio
-elif [ ! -f /tmp/Firefox-Quantum-Developer_amd64.tar.bz2 ]
+elif [ ! -f /tmp/Firefox-Developer_amd64.tar.bz2 ]
     then
         REINTENTOS=50
         echo -e "$verde Descargando$rojo Firefox Quantum Developer Edition$gris"
         for (( i=1; i<=$REINTENTOS; i++ ))
         do
-            rm -f /tmp/Firefox-Quantum-Developer_amd64.tar.bz 2>> /dev/null
-            wget --show-progress -r -A tar.bz2 'https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=es-ES' -O /tmp/Firefox-Quantum-Developer_amd64.tar.bz2 && break
+            rm -f /tmp/Firefox-Developer_amd64.tar.bz 2>> /dev/null
+            wget --show-progress -r -A tar.bz2 'https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=es-ES' -O /tmp/Firefox-Developer_amd64.tar.bz2 && break
         done
-        echo -e "$verde Preparando para instalar$rojo Firefox Quantum Developer Edition$gris"
+        echo -e "$verde Preparando para instalar$rojo Firefox Developer Edition$gris"
 
         instalar
     else
@@ -91,27 +91,27 @@ elif [ ! -f /tmp/Firefox-Quantum-Developer_amd64.tar.bz2 ]
         # Acceso directo
         desktop='
             [Desktop Entry]
-            Name=Firefox Quantum
-            Name[es]=Firefox Quantum
+            Name=Firefox Developer
+            Name[es]=Firefox Developer
             Comment=Browse the World Wide Web
             Comment[es]=Navegue por la web
             GenericName=Web Browser
             GenericName[es]=Navegador web
-            X-GNOME-FullName=Firefox Quantum Web Browser
-            X-GNOME-FullName[es]=Navegador web Firefox Quantum
-            Exec=.local/bin/firefox-quantum %u
+            X-GNOME-FullName=Firefox Developer Web Browser
+            X-GNOME-FullName[es]=Navegador web Firefox Developer
+            Exec=.local/bin/firefox-developer %u
             Terminal=false
             X-MultipleArgs=false
             Type=Application
             Icon=firefox-developer-icon
             Categories=Network;WebBrowser;
             MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/vnd.mozilla.xul+xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
-            StartupWMClass=Firefox-Quantum
+            StartupWMClass=Firefox-Developer
             StartupNotify=true
-            Name[es_ES]=firefox-quantum
+            Name[es_ES]=firefox-developer
         '
         # Copiar acceso directo
-        cat desktop > ~/.local/share/applications/firefox-quantum.desktop 2>> /dev/null
+        cat desktop > ~/.local/share/applications/firefox-developer.desktop 2>> /dev/null
     }
 
     accesos_Directos
