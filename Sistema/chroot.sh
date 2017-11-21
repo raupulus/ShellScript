d5661b37b5f4f4b4cf00a9c36ae468ba6b2df99f
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-11-21 17:55:40 +0100

    Planteada función crear jaula

--- Sistema/chroot.sh
+++ Sistema/chroot.sh
@@ -11,3 +11,23 @@
 #############################
 ##   Variables Generales   ##
 #############################
+
+# Enjaular en la ruta indicada
+function enjaular() {
+    while [ $jaula == '' ]
+    do
+        clear
+        read -p "Introduce la ruta donde enjaularte (¿/mnt?)" jaula
+    done
+
+    mount -t proc proc /$jaula/proc/
+    mount -t sysfs sys /$jaula/sys/
+    mount -o bind /dev/ /$jaula/dev
+    mount -t devpts pts /$jaula/dev/pts
+
+    chroot /$jaula/ /bin/bash
+    #export PS1="Enjaulado → (chroot) $PS1"
+}
+
+montarJaula
+enjaular
