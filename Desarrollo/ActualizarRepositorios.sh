0759360ed00f49de8453da9ae28d29a7bd5ca293
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-23 23:37:41 +0200

    Preparo script para actualizar repositorios GIT

---
+++ Desarrollo/ActualizarRepositorios.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2017 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
+
+############################
+##     INSTRUCCIONES      ##
+############################
+## Menú principal para instalar aplicaciones desde el que se permite
+## elegir entre los tipos de aplicaciones a instalar desde un menú
+## interactivo seleccionando el número que le corresponda
+
+############################
+##     IMPORTACIONES      ##
+############################
+DIRGIT=''
+DIRACTUAL="$(pwd)"
+
+############################
+##       FUNCIONES        ##
+############################
+
+echo $DIRACTUAL
+
+exit 0
