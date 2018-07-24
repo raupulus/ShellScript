e031e752feaf382e64c79c13fc2627e53ff39988
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-24 11:23:03 +0200

    Actualizo script contar líneas

--- MiniScripts/ContarLineas.sh
+++ MiniScripts/ContarLineas.sh
@@ -1,26 +1,51 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      dev@fryntiz.es
+## @web        https://fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
 
-#######################################
-# ###     Raúl Caro Pastorino     ### #
-## ##                             ## ##
-### # https://github.com/fryntiz/ # ###
-## ##                             ## ##
-# ###       www.fryntiz.es        ### #
-#######################################
+############################
+##     INSTRUCCIONES      ##
+############################
+## Este script contará las líneas de todos los archivos del directorio
 
-# Este script contará las líneas de todos los archivos del directorio
+############################
+##       CONSTANTES       ##
+############################
+AM="\033[1;33m"  ## Color Amarillo
+RO="\033[1;31m"  ## Color Rojo
+VE="\033[1;32m"  ## Color Verde
+CL="\e[0m"       ## Limpiar colores
 
+###########################
+##       VARIABLES       ##
+###########################
+
+###########################
+##       FUNCIONES       ##
+###########################
 TOTAL=0
 
-#Cuenta todas las líneas y las agrego a un array
+## TODO → Solo contar archivos con extensiones válidas a partir de una lista
+
+## Cuenta todas las líneas y las agrego a un array
 TMP=`find  -type f -exec wc -l {} \; | cut -d " " -f 1`
 
-#Bucle para recorrer todos los valores de $TMP
+## Bucle para recorrer todos los valores de $TMP
 for i in $TMP; do
-	TOTAL=$(( $TOTAL + $i ))
+    TOTAL=$(( $TOTAL + $i ))
 done
 
-#Mostrar resultado
+## Mostrar resultado
 echo "El total es $TOTAL"
 
 exit 0
