6ff09f39456ef07ef2c996fd220f56adf0636885
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-24 02:27:22 +0200

    Script para enjaular una distribución cifrada

---
+++ Sistema/chrootCifrado.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+
+
+
+AM="\033[1;33m"  AZ="\033[1;34m"  BL="\033[1;37m"  CY="\033[1;36m"  GR="\033[0;37m"  MA="\033[1;35m"  RO="\033[1;31m"  VE="\033[1;32m"  CL="\e[0m"       
+VERSION="0.0.0"
+WORKSCRIPT=$PWD  USER=$(whoami)   
+
+
+
+
+exit 0
