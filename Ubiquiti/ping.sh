17e202a94a86e615cb554a10d0ffc292dbeb6083
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-07-24 11:38:00 +0200

    Actualizo script para hacer ping

--- Ubiquiti/ping.sh
+++ Ubiquiti/ping.sh
@@ -1,36 +1,45 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      dev@fryntiz.es
+## @web        https://fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
 
-#Variables Generales
+############################
+##     INSTRUCCIONES      ##
+############################
+## 
+
+############################
+##       CONSTANTES       ##
+############################
+AM="\033[1;33m"  ## Color Amarillo
+RO="\033[1;31m"  ## Color Rojo
+VE="\033[1;32m"  ## Color Verde
+CL="\e[0m"       ## Limpiar colores
+
+###########################
+##       VARIABLES       ##
+###########################
 IP=$(cat ./ip.config)
 PING="ping -q -c12 $IP"
 FILTRO="grep packets"
 
-##### CONSTANTES COLORES #####
-negro="\033[0;30m"
-rojo="\033[0;31m"
-verde="\033[0;32m"
-marron="\033[0;33m"
-azul="\033[0;34m"
-magenta="\033[0;35m"
-cyan="\033[01;36m"
-grisC="\033[0;37m"
-gris="\033[1;30m"
-rojoC="\033[1;31m"
-verdeC="\033[1;32m"
-amarillo="\033[1;33m"
-azulC="\033[1;34m"
-magentaC="\033[1;35m"
-cyanC="\033[1;36m"
-blanco="\033[1;37m"
-subrayar="\E[4m"
-parpadeoON="\E[5m"
-parpadeoOFF="\E[0m"
-resaltar="\E[7m"
-
+###########################
+##       FUNCIONES       ##
+###########################
 clear
 
-echo -e "$rojoC La duración de esta prueba es aproximadamente$amarillo 10$rojoC segundos"
-echo -e "$azulC Comenzando prueba:$grisC"
+echo -e "$RO La duración de esta prueba es aproximadamente$AM 10$RO segundos"
+echo -e "$RO Comenzando prueba:$CL"
 sleep 1
-echo -e "$amarillo $($PING | $FILTRO) $grisC"
+echo -e "$AM $($PING | $FILTRO) $CL"
 echo ""
